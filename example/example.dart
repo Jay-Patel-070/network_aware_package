fb5e6f6cf2b61ffcbb8a902cee8e31269b64d92e
Jay <jayspatel420005@gmail.com>
2025-12-16 18:10:53 +0530

    made documentation proper

--- example/example.dart
+++ example/example.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:network_aware_package/network_aware_package.dart';
 
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  NetworkService.instance.initialize();
+  runApp(const MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
